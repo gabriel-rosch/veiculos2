84344e6f33212e5623182ee467f57797e10ddb21
gabriel.rosch <gabriel.rosch@veolia.com>
2023-11-29 20:59:26 -0300

    ultima-aula

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import { Button, Form, Table } from 'react-bootstrap';
 
 function App() {
-  const [placa, setPlaca] = useState('')
-  const [modelo, setModelo] = useState('')
+  const [placa, setPlaca] = useState('BRA2E19')
+  const [modelo, setModelo] = useState('vw')
   const [ano, setAno] = useState(2023)
   const [cor, setCor] = useState('')
 
@@ -28,12 +28,35 @@ function App() {
 
     var regex = '[A-Z]{3}[0-9][0-9A-Z][0-9]{2}';
     
-    if (!placa.match(regex)) {
+    if (!placa.match(regex)) 
+    {
       alert('Placa invalida!')
-    } else {
+    } 
+    else if(veiculo.modelo.length == 0)
+    {
+      alert('Modelo é obrigatório!')
+    }
+    else
+    {
       veiculos.push(veiculo)
       setVeiculos([...veiculos])
+      limpar()
     }
+  }
+
+  function excluir(placa) {
+    let indexExluir = null;
+
+    for (let i = 0; i < veiculos.length; i++) {
+      let veiculo = veiculos[i];
+      if(veiculo.placa == placa) {
+        indexExluir = i
+      }
+    }
+
+    veiculos.splice(indexExluir, 1)
+    setVeiculos([...veiculos])
+
   }
 
   return (
@@ -75,6 +98,7 @@ function App() {
               <th>Modelo</th>
               <th>Ano</th>
               <th>Cor</th>
+              <th>Ação</th>
             </tr>
           </thead>
           <tbody>
@@ -86,6 +110,7 @@ function App() {
                     <td>{veiculo.modelo}</td>
                     <td>{veiculo.ano}</td>
                     <td>{veiculo.cor}</td>
+                    <td><Button onClick={()=>{excluir(veiculo.placa)}}>X</Button></td>
                   </tr>
                 )
               })
